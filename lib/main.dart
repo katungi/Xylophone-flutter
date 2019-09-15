899c9ce3e9bcca232240205e1d2e6f081d0cd34b
katungi <dankatdennis@gmail.com>
2019-09-15 19:41:23 +0300

    changed colors

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,21 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-
   void playSound(int soundNumber) {
     final player = AudioCache();
     player.play('note$soundNumber.wav');
   }
 
-  Expanded buildKey( {Color color, int soundNumber}){  
-       return Expanded(
-            child: FlatButton(
-            color: color,
-              onPressed: (){
-                playSound(soundNumber);
-               } 
-              ),
-            );       
-     }
+  Expanded buildKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+          color: color,
+          onPressed: () {
+            playSound(soundNumber);
+          }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,17 +29,17 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              buildKey(color: Colors.redAccent, soundNumber:1),
-              buildKey(color: Colors.yellow[200], soundNumber:2),
-              buildKey(color: Colors.tealAccent, soundNumber:3),
-              buildKey(color: Colors.purple[200], soundNumber:4),
-              buildKey(color: Colors.grey, soundNumber:5),
-              buildKey(color: Colors.blueAccent, soundNumber:6),
-              buildKey(color: Colors.orange[200], soundNumber:7)
-          ],
+              buildKey(color: Colors.redAccent, soundNumber: 1),
+              buildKey(color: Colors.yellow[200], soundNumber: 2),
+              buildKey(color: Colors.tealAccent, soundNumber: 3),
+              buildKey(color: Colors.purple[200], soundNumber: 4),
+              buildKey(color: Colors.grey, soundNumber: 5),
+              buildKey(color: Colors.blueAccent, soundNumber: 6),
+              buildKey(color: Colors.orange[200], soundNumber: 7)
+            ],
+          ),
         ),
       ),
-    ),
     );
   }
 }
