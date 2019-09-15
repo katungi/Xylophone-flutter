5f7a28978162e45e49ccb7275d8bf49f57ae5934
katungi <dankatdennis@gmail.com>
2019-09-15 18:16:45 +0300

    added more buttons with tones onpress

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -8,9 +9,61 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+            FlatButton(
+              color: Colors.amber,
+              onPressed: (){
+                final player = AudioCache();
+                player.play('note1.wav');
+              } 
+            ),
+            FlatButton(
+              color: Colors.pink,
+              onPressed: (){
+                final player = AudioCache();
+                player.play('note2.wav');
+              } 
+            ),
+            FlatButton(
+              color: Colors.blue,
+              onPressed: (){
+                final player = AudioCache();
+                player.play('note3.wav');
+              } 
+            ),
+            FlatButton(
+              color: Colors.purple,
+              onPressed: (){
+                final player = AudioCache();
+                player.play('note4.wav');
+              } 
+            ),
+            FlatButton(
+              color: Colors.teal,
+              onPressed: (){
+                final player = AudioCache();
+                player.play('note5.wav');
+              } 
+            ),
+            FlatButton(
+              color: Colors.amber,
+              onPressed: (){
+                final player = AudioCache();
+                player.play('note6.wav');
+              } 
+            ),
+            FlatButton(
+              color: Colors.green,
+              onPressed: (){
+                final player = AudioCache();
+                player.play('note7.wav');
+              } 
+            ),
+          ],
         ),
       ),
+    ),
     );
   }
 }
